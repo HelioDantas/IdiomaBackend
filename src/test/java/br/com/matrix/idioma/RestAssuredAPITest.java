751e0eb9c4afe02994dcae140698a99a9667caf2
helio <helio_dantas@outlook.com>
2018-10-10 16:11:48 -0300

    teste de usuario

--- src/test/java/br/com/matrix/idioma/RestAssuredAPITest.java
+++ src/test/java/br/com/matrix/idioma/RestAssuredAPITest.java
@@ -5,7 +5,7 @@ import io.restassured.RestAssured.*;
 import io.restassured.matcher.RestAssuredMatchers.*;
 
 import static org.assertj.core.api.Assertions.not;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.hamcrest.Matchers.*;
@@ -39,4 +39,17 @@ public class RestAssuredAPITest {
 			body("englishTranscription", notNullValue()).
 			body("portugueseTranscription", notNullValue());										
 	}
+	@Test
+	public void givenUrl_whenSuccessOnGetsResponseAndJsonHasRequiredKV_thenCorrect() {
+		RestAssured.given().
+		when().
+			get("/user/1").
+			then().statusCode(200)
+			.body("email", containsString("mario@unicarioca.edu.br"))
+			.body("login", containsString("mario"))
+			.body("name", containsString("Mario"))
+			.body("password", containsString("123456"));
+			; 
+	}
+	
 }

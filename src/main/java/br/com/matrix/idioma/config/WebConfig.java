43ee3e405d4ef15cc95d7d8582bec95c6e843c63
wellingtonconsilv <wellingtonconsilv@gmail.com>
2018-10-11 19:41:27 -0300

    Métodos GET, POST, PUT, DELETE E OPTIONS habilitados no cors

--- src/main/java/br/com/matrix/idioma/config/WebConfig.java
+++ src/main/java/br/com/matrix/idioma/config/WebConfig.java
@@ -1,20 +1,18 @@
 package br.com.matrix.idioma.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**")
+				.allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS");
 			}
-		};
-	}
+	
 }	

7e75685a2b17760ab57b28c84128519a2714b060
wellingtonconsilv <wellingtonconsilv@gmail.com>
2018-10-11 23:18:45 -0300

    Modificações no cors, retirado implementacao do WebMvcConfigurer

--- src/main/java/br/com/matrix/idioma/config/WebConfig.java
+++ src/main/java/br/com/matrix/idioma/config/WebConfig.java
@@ -1,18 +1,21 @@
 package br.com.matrix.idioma.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS");
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD");
 			}
-	
+		};
+	}
 }	
